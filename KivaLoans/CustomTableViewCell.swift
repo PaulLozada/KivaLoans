af8f70cf5b011cd1382ccd6d6da09801909436c1
PaulLozada <paul_lozada@icloud.com>
2016-02-15 23:31:31 -0700

    First commit

---
+++ KivaLoans/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  KivaLoans
+//
+//  Created by Paul Lozada on 2016-02-15.
+//  Copyright © 2016 Paul Lozada. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
